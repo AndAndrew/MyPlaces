ac4071c8d4a47e6c9d9f3c71c69b584da974ef52
AndAndrew <andrew.krivokhizhin@gmail.com>
2020-04-09 13:19:47 +0400

    CustomCell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Andrew K on 09.04.2020.
+//  Copyright © 2020 Andrew K. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlaces: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
---
+++ MyPlaces/TableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  TableViewController.swift
+//  MyPlaces
+//
+//  Created by Andrew K on 06.04.2020.
+//  Copyright © 2020 Andrew K. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    let restaurantNames = ["Harats", "Guitar Bar", "Na Dne", "Prolivoshnaya", "Gellert", "Jaw Pike"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return restaurantNames.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
+
+        cell.nameLabel.text = restaurantNames[indexPath.row]
+        cell.imageOfPlaces.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageOfPlaces.layer.cornerRadius = cell.imageOfPlaces.frame.size.height / 2
+        cell.imageOfPlaces.clipsToBounds = true
+
+        return cell
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
